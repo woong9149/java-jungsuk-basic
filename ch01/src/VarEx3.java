1b9b170603fa2df9de29aa24baf422571489972e
woong9149 <sewoong0409@naver.com>
2022-01-11 23:56:36 +0900

    20220111_1

---
+++ ch01/src/VarEx3.java
@@ -0,0 +1,11 @@
+
+public class VarEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int score = 100;
+		//지역변수는 사용하기 전에 초기화를 해야한다.
+		System.out.println(score);
+	}
+
+}
