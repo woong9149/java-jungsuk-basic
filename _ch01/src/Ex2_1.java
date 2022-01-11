3bead2ce81e3f8ba250bd9cfe29ff07d15b2cfb2
woong9149 <sewoong0409@naver.com>
2022-01-11 09:39:49 +0900

    20220111

---
+++ _ch01/src/Ex2_1.java
@@ -0,0 +1,15 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 6, b = 3;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		
+	}
+
+}
