2bb252e50dabe5328db7b9075b7f676f24a39a1c
woong9149 <sewoong0409@naver.com>
2022-01-16 21:51:48 +0900

    20220116

---
+++ ch06/src/Ex6.java
@@ -0,0 +1,18 @@
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 *  객체지향 언어
+		 *  코드의 재사용성이 높고, 유지보수가 용이하며 중복 코드를 제하는 기술들이 들어가 있다.
+		 *  => 빠른 변화에 따라가기 쉽도록 개념과 규칙들이 추가된 것.
+		 *  1. 캡슐화
+		 *  2. 상속
+		 *  3. 추상화
+		 *  4. 다형성 
+		 */
+
+	}
+
+}
