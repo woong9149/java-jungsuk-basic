bd7d01972ada19cbaaf7565503abf3d906c2344e
woong9149 <sewoong0409@naver.com>
2022-02-09 23:14:28 +0900

    220209

--- ch06/src/Ex7_9.java
+++ ch06/src/Ex7_9.java
@@ -6,7 +6,13 @@ public class Ex7_9 {
 	/**
 	 * 	생성자 this()
 	 *  - 생성자에서 다른 생성자 호출할 때 사용
-	 *  - 다른 생성자 호출시 첫 줄에서만 사용 가
+	 *  - 다른 생성자 호출시 첫 줄에서만 사용 가능
+	 *  
+	 *  참조변수 this
+	 *  - 인스턴스 자신을 가리키는 참조 변수(인스턴스의 주소가 저장되어있음)
+	 *  - 인스턴스 메서드(생성자 포함)에서 사용 가능
+	 *  - 지역변수(lv)와 인스턴스 변수(iv)를 구별할 때 사용
+	 *  - static매서드(클래스 메서드)에서는 사용불가
 	 */
 	}
 
@@ -26,6 +32,8 @@ class Car2{
 	}
 	
 	Car2(String color, String gearType, int door) {
+		//this는 참조변수 this
+		//생략해도 오류는 안나지만, 로컬변수로 취급받음.
 		this.color = color;
 		this.gearType = gearType;
 		this.door = door;
