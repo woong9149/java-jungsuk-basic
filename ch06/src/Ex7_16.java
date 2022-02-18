ee1b58ca0b81feaf602d20fc4e0c659044776d1f
woong9149 <sewoong0409@naver.com>
2022-02-18 23:52:41 +0900

    20220218

---
+++ ch06/src/Ex7_16.java
@@ -0,0 +1,22 @@
+
+public class Ex7_16 {
+
+	public static void main(String[] args) {
+		/**
+		 *  패키지(package)
+		 *  - 서로 관련 있는 클래스들의 묶음
+		 *  - 클래스는 클래스파(*.class), 패키지는 폴더, 하위 패키지는 하위 폴더
+		 *  - 클래스의 실제 이름(full name)은 패키지를 포함한다. ex) java.lang.String
+		 *  - 클래스들을 압축한 파일이 rt.jar(java9 부터는 모듈이 도입되서 없어짐)
+		 *  
+		 *  패키지의 선언
+		 *  - 패키지는 소스 파일의 첫번째 문장으로 단 한번 선언
+		 *  - 같은 소스 파일의 클래스들은 모두 같은 패키지에 속하게 된다.
+		 *  - 패키지 선언이 없으면 이름 없는(unnamed, default package) 패키지에 속하게 된다.
+		 *  
+		 */
+		
+
+	}
+
+}
