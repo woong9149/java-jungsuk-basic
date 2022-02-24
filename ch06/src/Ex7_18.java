c23330fe0bf788612d82161d81548ccbf8e9e985
woong9149 <sewoong0409@naver.com>
2022-02-24 23:54:16 +0900

    20220224_2

---
+++ ch06/src/Ex7_18.java
@@ -0,0 +1,15 @@
+
+public class Ex7_18 {
+
+	public static void main(String[] args) {
+		/**
+		 *  접근 제어자(access modifier)
+		 *  - private: 같은 클래스 내에서만 접근이 가능하다.(대상 - 클래스)
+		 *  - (default): 같은 패키지 내에서만 접근이 가능하다. default는 따로 명시하지 않음. (대상 - 클래스)
+		 *  - protected: 같은 패키지 내에서, 그리고 다른 패키지의 자손 클래스에서 접근이 가능하다. (대상 - 클래스, 멤버)
+		 *  - public: 접근 제한이 전혀 없다. (대상 - 클래스, 멤버)
+		 *  
+		 */
+		
+	}
+}
