83cecffa866015702ad50bc1d92da81972642817
woong9149 <sewoong0409@naver.com>
2023-06-26 23:46:53 +0900

    230626 java basic

--- ch06/src/Ex6_10.java
+++ ch06/src/Ex6_10.java
@@ -41,3 +41,16 @@ class MyMath3 {
         return result;
     }
 }
+
+class StaticTest {
+    static int width = 200; // 클래스 변수(static 변수)
+    static int height = 120; // 클래스 변수(static 변수)
+
+    static {
+        // static 변수의 복잡한 초기화 수행
+    }
+
+    static int max (int a, int b) { // 클래스 메서드(static메서드)
+         return a > b ? a : b;
+    }
+}
---
+++ ch06/src/TimeTest.java
@@ -0,0 +1,11 @@
+public class Time {
+    int hour;
+    int minute;
+    int second;
+}
+
+public class TimeTest {
+    public static void main(String[] args) {
+        
+    }
+        }
